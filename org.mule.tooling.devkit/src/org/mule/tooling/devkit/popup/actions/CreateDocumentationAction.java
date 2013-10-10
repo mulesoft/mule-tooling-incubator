8e0a214c9ce16ba1930e502413be45f4aaf6324b
fernandofederico1984 <fernandofederico1984@gmail.com>
2013-10-10 15:24:08 -0300

    adding devkit changes

--- org.mule.tooling.devkit/src/org/mule/tooling/devkit/popup/actions/CreateDocumentationAction.java
+++ org.mule.tooling.devkit/src/org/mule/tooling/devkit/popup/actions/CreateDocumentationAction.java
@@ -1,5 +1,7 @@
 package org.mule.tooling.devkit.popup.actions;
 
+import java.net.MalformedURLException;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.WorkspaceJob;
 import org.eclipse.core.runtime.CoreException;
@@ -7,44 +9,62 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.help.ui.internal.DefaultHelpUI;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 import org.mule.tooling.devkit.maven.BaseDevkitGoalRunner;
 import org.mule.tooling.devkit.maven.MavenDevkitProjectDecorator;
+import org.mule.tooling.ui.widgets.util.SilentRunner;
 
-public class GenerateSourcesAction implements IObjectActionDelegate {
+@SuppressWarnings("restriction")
+public class CreateDocumentationAction implements IObjectActionDelegate {
 
     /** Current selection */
     private IStructuredSelection selection;
-
-    /** Current workbench part */
 
     public void setActivePart(IAction action, IWorkbenchPart targetPart) {
 
     }
 
     public void run(IAction action) {
-
-    	   
         Object selected = selection.getFirstElement();
         if (selected instanceof IJavaElement) {
             final IProject selectedProject = ((IJavaElement) selected).getJavaProject().getProject();
             if (selectedProject != null) {
 
-                final String convertingMsg = "Generating Sources...";
+                final String convertingMsg = "Generating Documentation...";
                 final WorkspaceJob changeClasspathJob = new WorkspaceJob(convertingMsg) {
 
                     @Override
                     public IStatus runInWorkspace(final IProgressMonitor monitor) throws CoreException {
                         monitor.beginTask(convertingMsg, 100);
                         MavenDevkitProjectDecorator mavenProject = MavenDevkitProjectDecorator.decorate(JavaCore.create(selectedProject));
-                        new BaseDevkitGoalRunner(new String[] { "clean", "package", "-DskipTests", "-Ddevkit.studio.package.skip=true", "-Ddevkit.javadoc.check.skip=true",
-                                "-Dmaven.javadoc.skip=true" }).run(mavenProject.getPomFile(), monitor);
+                        new BaseDevkitGoalRunner(new String[] { "clean", "package", "-DskipTests", "javadoc:javadoc" }).run(mavenProject.getPomFile(), monitor);
+
+                        Display.getDefault().syncExec(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                SilentRunner.run(new Runnable() {
+
+                                    @Override
+                                    public void run() {
+                                        try {
+                                            DefaultHelpUI.showInWorkbenchBrowser(selectedProject.getFile("/target/apidocs/index.html").getLocationURI().toURL().toString(), true);
+                                        } catch (MalformedURLException e) {
+                                            throw new RuntimeException(e);
+                                        }
+
+                                    }
+                                });
+                            }
+                        });
 
                         return Status.OK_STATUS;
                     }
---
+++ org.mule.tooling.devkit/src/org/mule/tooling/devkit/popup/actions/SendToPaletteAction.java
@@ -0,0 +1,136 @@
+package org.mule.tooling.devkit.popup.actions;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.jar.JarFile;
+
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.WorkspaceJob;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+import org.mule.tooling.core.MuleCorePlugin;
+import org.mule.tooling.core.utils.BundleJarFileInspector;
+import org.mule.tooling.core.utils.BundleManifestReader;
+import org.mule.tooling.devkit.DevkitUIPlugin;
+import org.mule.tooling.devkit.maven.BaseDevkitGoalRunner;
+import org.mule.tooling.devkit.maven.MavenDevkitProjectDecorator;
+import org.mule.tooling.messageflow.editor.MultiPageMessageFlowEditor;
+import org.mule.tooling.messageflow.util.MessageFlowUtils;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
+
+public class SendToPaletteAction implements IObjectActionDelegate {
+
+	private IStructuredSelection selection;
+
+	
+	private static Bundle findBundle(BundleContext bundleContext, String symbolicName ){
+		Bundle[] bundles = bundleContext.getBundles();
+		for (Bundle bundle : bundles) {
+			if ( bundle.getSymbolicName().equals(symbolicName) ) {
+				return bundle;
+			}
+		}
+		return null;
+	}
+	
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {}
+
+	public void run(IAction action) {
+		
+		Object selected = selection.getFirstElement();
+		if (selected instanceof IJavaElement) {
+			final IProject selectedProject = ((IJavaElement) selected).getJavaProject().getProject();
+			if (selectedProject != null) {
+
+				final String installingPalette = "Installing into palette...";
+				final WorkspaceJob sendToPalette = new WorkspaceJob(installingPalette) {
+
+					@Override
+					public IStatus runInWorkspace(final IProgressMonitor monitor) throws CoreException {
+						monitor.beginTask(installingPalette, 100);
+						MavenDevkitProjectDecorator mavenProject = MavenDevkitProjectDecorator.decorate(JavaCore.create(selectedProject));
+
+						new BaseDevkitGoalRunner(new String[] { "clean", "package", "-DskipTests", "-Ddevkit.studio.package.skip=false" }).run(mavenProject.getPomFile(), monitor);
+						IFolder pluginsDirectory = selectedProject.getProject().getFolder("/target/update-site/plugins/");
+						File pluginsFolder = new File(pluginsDirectory.getLocationURI());
+						if ( pluginsFolder.exists() )
+						{
+							for ( File resource : pluginsFolder.listFiles()){
+								installBundle(resource);
+							}
+						}
+						return Status.OK_STATUS;
+					}
+				};
+				sendToPalette.setUser(true);
+				sendToPalette.setPriority(Job.SHORT);
+				sendToPalette.schedule();
+			}
+		}
+	}
+
+	private void installBundle(File resource) {
+		try {
+
+			BundleManifestReader manifestReader = new BundleJarFileInspector(new JarFile(resource)).getManifest();
+			BundleContext bundleContext = DevkitUIPlugin.getDefault().getBundle().getBundleContext();
+			String location = resource.toURI().toString();
+
+			Bundle bundle = findBundle(bundleContext, manifestReader.getSymbolicName());
+			
+			if ( bundle != null ){
+				bundle.update(new FileInputStream(resource));
+			}
+			else{
+				bundle = bundleContext.installBundle(location);
+			}
+				
+			bundle.start();
+
+			reloadPalette();
+		} catch (BundleException e) {
+			// TODO: Show error
+		} catch (FileNotFoundException e) {
+			// TODO: Show error
+		} catch (IOException e) {
+			// TODO: Show error
+		}
+	}
+
+	private void reloadPalette() {
+		Display display = Display.getCurrent();
+		if(display == null){
+			display = Display.getDefault();
+		}
+		display.asyncExec(new Runnable() {
+
+			@Override
+			public void run() {
+				MuleCorePlugin.getModuleManager().reinit();
+			}
+		});
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+		if (selection instanceof IStructuredSelection) {
+			this.selection = (IStructuredSelection) selection;
+		}
+	}
+	
+}
