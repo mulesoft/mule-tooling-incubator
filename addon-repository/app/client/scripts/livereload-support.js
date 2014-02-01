278b5cb398f5b7c3c2cbcad9398726782e6919a0
Mariano de Achaval <mariano.achaval@mulesoft.com>
2014-02-01 16:32:23 -0300

    Cleaning incubators

--- addon-repository/app/client/scripts/livereload-support.js
+++
@@ -1,6 +0,0 @@
-(function () {
-	/* global document */
-	'use strict';
-
-	document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js"></' + 'script>');
-})();
