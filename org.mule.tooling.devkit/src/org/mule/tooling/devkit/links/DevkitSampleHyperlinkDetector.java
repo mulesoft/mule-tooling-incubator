678147e4dcc1d74449a8a04294e931796283f5d8
pablocabrera85 <pablo.cabrera@mulesoft.com>
2014-03-18 19:05:11 -0700

    Add links to samples

---
+++ org.mule.tooling.devkit/src/org/mule/tooling/devkit/links/DevkitSampleHyperlinkDetector.java
@@ -0,0 +1,69 @@
+package org.mule.tooling.devkit.links;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.ITypeRoot;
+import org.eclipse.jdt.internal.ui.JavaPlugin;
+import org.eclipse.jdt.internal.ui.javaeditor.JavaEditor;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.hyperlink.AbstractHyperlinkDetector;
+import org.eclipse.jface.text.hyperlink.IHyperlink;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.part.FileEditorInput;
+import org.eclipse.ui.texteditor.ITextEditor;
+import org.mule.tooling.devkit.common.DevkitUtils;
+
+public class DevkitSampleHyperlinkDetector extends AbstractHyperlinkDetector {
+
+	Pattern pattern = Pattern.compile(".*\\{@sample.*\\}");
+
+	@Override
+	public IHyperlink[] detectHyperlinks(ITextViewer textViewer,
+			IRegion region, boolean canShowMultipleHyperlinks) {
+		ITextEditor textEditor = (ITextEditor) getAdapter(ITextEditor.class);
+
+		if (textEditor == null || !(textEditor instanceof JavaEditor))
+			return null;
+
+		IDocument document = textEditor.getDocumentProvider().getDocument(
+				textEditor.getEditorInput());
+		FileEditorInput fe = (FileEditorInput) textEditor.getEditorInput();
+		IFolder folder = fe.getFile().getProject()
+				.getFolder(DevkitUtils.DOCS_FOLDER);
+		
+		try {
+			IRegion reg = document.getLineInformationOfOffset(region
+					.getOffset());
+			String value = document.get(reg.getOffset(), reg.getLength());
+			Matcher matcher = pattern.matcher(value);
+			if (matcher.find()) {
+				IHyperlink[] links = new IHyperlink[1];
+				int offset = value.indexOf("{@sample");
+				int lastChar = value.indexOf("}");
+				links[0] = new SampleHyperlink(reg.getOffset() + offset,
+						(lastChar - offset) + 1, "Link to devkit Sample",
+						"Go to sample", value.substring(value.indexOf("@"),
+								value.indexOf("}")), fe.getFile().getProject());
+				return links;
+			}
+			System.out.println(value);
+		} catch (BadLocationException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
---
+++ org.mule.tooling.devkit/src/org/mule/tooling/devkit/links/SampleHyperlink.java
@@ -0,0 +1,125 @@
+package org.mule.tooling.devkit.links;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.Region;
+import org.eclipse.jface.text.hyperlink.IHyperlink;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
+import org.mule.tooling.devkit.common.DevkitUtils;
+
+public class SampleHyperlink implements IHyperlink {
+
+	private final IRegion region;
+	private final String label;
+	private final String text;
+	private final String link;
+	private final IProject project;
+
+	public SampleHyperlink(int offset, int length, String label, String text,
+			String link, IProject project) {
+		this(new Region(offset, length), label, text, link, project);
+
+	}
+
+	public SampleHyperlink(IRegion region, String label, String text,
+			String link, IProject project) {
+		this.region = region;
+		this.label = label;
+		this.text = text;
+		this.link = link;
+		this.project = project;
+	}
+
+	@Override
+	public IRegion getHyperlinkRegion() {
+		return region;
+	}
+
+	@Override
+	public String getTypeLabel() {
+		return label;
+	}
+
+	@Override
+	public String getHyperlinkText() {
+		return text;
+	}
+
+	@Override
+	public void open() {
+		IFolder folder = project.getFolder(DevkitUtils.DOCS_FOLDER);
+		IFile file = null;
+		try {
+			for (IResource resource : folder.members()) {
+				if (resource.getName().matches(".*.sample")) {
+					file = project.getFile(resource.getProjectRelativePath());
+					break;
+				}
+			}
+		} catch (CoreException e1) {
+			e1.printStackTrace();
+		}
+		InputStreamReader isr = null;
+		try {
+			int startPos = link.indexOf("@sample.xml");
+			String sample = link.substring(startPos).split(" ")[2];
+			isr = new InputStreamReader(file.getContents());
+			BufferedReader ir = new BufferedReader(isr);
+			int lineNumber = 0;
+			String line;
+			boolean found = false;
+			while ((line = ir.readLine()) != null) {
+				lineNumber++;
+				if (line.contains(sample)) {
+					found = true;
+					break;
+				}
+
+			}
+			if (!found) {
+				lineNumber = 0;
+			}
+			HashMap map = new HashMap();
+			map.put(IMarker.LINE_NUMBER, new Integer(lineNumber));
+			map.put(IWorkbenchPage.EDITOR_ID_ATTR,
+					"org.mule.tooling.devkit.sample.editor.editors.XMLEditor");
+			IMarker marker;
+
+			marker = file.createMarker(IMarker.TEXT);
+
+			marker.setAttributes(map);
+			// page.openEditor(marker); //2.1 API
+			IWorkbenchPage page = PlatformUI.getWorkbench()
+					.getActiveWorkbenchWindow().getActivePage();
+			// page.openEditor(new SampleDocEditorInput(file, module),
+			// "org.mule.tooling.devkit.sample.editor.editors.XMLEditor");
+			IDE.openEditor(page, marker); // 3.0 API
+			marker.delete();
+		} catch (CoreException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (isr != null) {
+				try {
+					isr.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+}
