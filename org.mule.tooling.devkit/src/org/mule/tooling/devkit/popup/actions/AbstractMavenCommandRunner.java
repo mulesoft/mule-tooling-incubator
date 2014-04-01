d5b5b16f33a01c34db2f34560e928ba12d0547a5
AleGMarra <alemarra@gmail.com>
2014-04-01 17:24:58 -0300

    Created an abstract maven runner to handler project errors and console. Must inheret from it and override doCommandJobOnProject

---
+++ org.mule.tooling.devkit/src/org/mule/tooling/devkit/popup/actions/AbstractMavenCommandRunner.java
@@ -0,0 +1,121 @@
+package org.mule.tooling.devkit.popup.actions;
+
+import org.apache.commons.lang.NotImplementedException;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.WorkspaceJob;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.mule.tooling.devkit.maven.BaseDevkitGoalRunner;
+import org.mule.tooling.devkit.maven.MavenDevkitProjectDecorator;
+
+public abstract class AbstractMavenCommandRunner extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		
+		ISelection selection = HandlerUtil.getActiveWorkbenchWindow(event)
+				.getActivePage().getSelection();
+		
+		if (selection != null & selection instanceof IStructuredSelection) {
+			Object selected = ((IStructuredSelection) selection)
+					.getFirstElement();
+
+			if (selected instanceof IJavaElement) {
+				final IProject selectedProject = ((IJavaElement) selected)
+						.getJavaProject().getProject();
+				if (selectedProject != null) {
+					int errorCount = countErrors(selectedProject);
+
+					if (errorCount > 0) {
+						boolean ignoreErrors = errorHandling(errorCount);
+						if (!ignoreErrors) {
+							return null;
+						}
+					}
+
+					doCommandJobOnProject(selectedProject);
+				}
+			}
+		}
+		return null;
+	}
+
+	
+	protected abstract void doCommandJobOnProject(final IProject selectedProject);
+
+	
+	protected void runMavenGoalJob(final IProject selectedProject,
+								   final String[] mavenCommand,
+								   final String jobMsg) {
+		
+		final WorkspaceJob changeClasspathJob = new WorkspaceJob(
+				jobMsg) {
+
+			@Override
+			public IStatus runInWorkspace(final IProgressMonitor monitor)
+					throws CoreException {
+				monitor.beginTask(jobMsg, 100);
+				IJavaProject javaProject= JavaCore.create(selectedProject);
+				MavenDevkitProjectDecorator mavenProject = MavenDevkitProjectDecorator
+						.decorate(javaProject);
+
+				new BaseDevkitGoalRunner(mavenCommand ,javaProject)
+						.run( mavenProject.getPomFile(), monitor);
+
+				return Status.OK_STATUS;
+			} 
+		};
+		changeClasspathJob.setUser(true);
+		changeClasspathJob.setPriority(Job.SHORT);
+		changeClasspathJob.schedule();
+	}
+
+	
+	protected boolean errorHandling(int errorCount) {
+		String errorText = "Your project has (" + errorCount + ") "
+				+ ((errorCount > 1) ? "errors" : "error" + ".");
+		boolean result = MessageDialog
+				.openConfirm(
+						null,
+						"Warning",
+						errorText
+						+ "\n\nDo you want to continue with this operation?.");
+		return result;
+	}
+
+	
+	private int countErrors(final IProject selectedProject) {
+		int errorCount = 0;
+		IMarker[] errors;
+		try {
+			errors = selectedProject.findMarkers(null /* all markers */,
+					true, IResource.DEPTH_INFINITE);
+			for (IMarker error : errors) {
+				int severity = error.getAttribute(IMarker.SEVERITY,
+						Integer.MAX_VALUE);
+				if (severity == IMarker.SEVERITY_ERROR) {
+					errorCount++;
+				}
+			}
+		} catch (CoreException e1) {
+			e1.printStackTrace();
+		}
+		return errorCount;
+	}
+
+}
--- org.mule.tooling.devkit/src/org/mule/tooling/devkit/popup/actions/GenerateInteropTestDataCommand.java
+++ org.mule.tooling.devkit/src/org/mule/tooling/devkit/popup/actions/GenerateInteropTestDataCommand.java
@@ -2,144 +2,62 @@ package org.mule.tooling.devkit.popup.actions;
 
 import java.util.Map;
 
-import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.WorkspaceJob;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.jdt.core.IJavaElement;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.handlers.HandlerUtil;
 import org.mule.tooling.devkit.dialogs.TestdataOptionsSelectionDialog;
-import org.mule.tooling.devkit.maven.BaseDevkitGoalRunner;
-import org.mule.tooling.devkit.maven.MavenDevkitProjectDecorator;
 
-public class GenerateInteropTestDataCommand extends AbstractHandler {
-
-	private String credentialsFile;
-	private String outputName;
+public class GenerateInteropTestDataCommand extends AbstractMavenCommandRunner {
 
 	private final String generateAll = "all";
 	private final String generateInterop = "interop";
 	private final String generateFunctional = "functional";
 	
 	private Map<String, String> configKeys = null;
+
 	
 	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		ISelection selection = HandlerUtil.getActiveWorkbenchWindow(event)
-				.getActivePage().getSelection();
-		if (selection != null & selection instanceof IStructuredSelection) {
-			Object selected = ((IStructuredSelection) selection)
-					.getFirstElement();
-
-			if (selected instanceof IJavaElement) {
-				final IProject selectedProject = ((IJavaElement) selected)
-						.getJavaProject().getProject();
-				if (selectedProject != null) {
-					int errorCount = 0;
-					IMarker[] errors;
-					try {
-						errors = selectedProject.findMarkers(null /* all markers */,
-								true, IResource.DEPTH_INFINITE);
-						for (IMarker error : errors) {
-							int severity = error.getAttribute(IMarker.SEVERITY,
-									Integer.MAX_VALUE);
-							if (severity == IMarker.SEVERITY_ERROR) {
-								errorCount++;
-							}
-						}
-					} catch (CoreException e1) {
-						e1.printStackTrace();
-					}
-
-					if (errorCount > 0) {
-						String errorText = "Your project has (" + errorCount + ") "
-								+ ((errorCount > 1) ? "errors" : "error" + ".");
-						boolean result = MessageDialog
-								.openConfirm(
-										null,
-										"Warning",
-										errorText
-												+ "\n\nDo you want to continue with this operation?.");
-						if (!result) {
-							return null;
-						}
-					}
-					
-					
-					if( setConfigurationAndContinue() ){
-
-						final String convertingMsg = "Generating Sources...";
-						final WorkspaceJob changeClasspathJob = new WorkspaceJob(
-								convertingMsg) {
+	protected void doCommandJobOnProject(final IProject selectedProject) {
+		if(getConfigurationAndContinue() ){
 	
-							@Override
-							public IStatus runInWorkspace(final IProgressMonitor monitor)
-									throws CoreException {
-								monitor.beginTask(convertingMsg, 100);
-								IJavaProject javaProject= JavaCore.create(selectedProject);
-								MavenDevkitProjectDecorator mavenProject = MavenDevkitProjectDecorator
-										.decorate(javaProject);
-					
-								new BaseDevkitGoalRunner(new String[] { 
-										"clean", "package",
-										"-DskipTests",
-										"-P", "testdata-generator",
-										"-Dtestdata.type="+getGenerationType(),
-										"-Dtestdata.replaceAll="+getGenerationType(),
-										"-Dtestdata.credentialsFile="+configKeys.get("credentialsFile"),
-										"-Dtestdata.outputFile="+configKeys.get("outputFile")}
-	                                    ,javaProject).run( mavenProject.getPomFile(), monitor);
+			final String[] mavenCommand = new String[] { 
+					"clean", "package",
+					"-DskipTests",
+					"-P", "testdata-generator",
+					"-Dtestdata.type="+getGenerationType(),
+					"-Dtestdata.replaceAll="+configKeys.get("replaceAll"),
+					"-Dtestdata.credentialsFile="+configKeys.get("credentialsFile"),
+					"-Dtestdata.outputFile="+configKeys.get("outputFile")};
+			
+			final String jobMsg = "Generating Sources...";
+			
+			runMavenGoalJob(selectedProject, mavenCommand, jobMsg);
 	
-	        							return Status.OK_STATUS;
-	        						} 
-	        					};
-	        					changeClasspathJob.setUser(true);
-	        					changeClasspathJob.setPriority(Job.SHORT);
-	        					changeClasspathJob.schedule();
-	
-	        				}
-	        			}
-					}
-        		}
-        		return null;
-        	}
-	
-	private String getGenerationType(){
-		
-		Boolean selectedInterop = new Boolean(configKeys.get("selectedInterop"));
-		Boolean selectedFunctional = new Boolean(configKeys.get("selectedFunctional"));
-		
-		if(selectedFunctional && selectedInterop)
-			return generateAll;
-		
-		if ( selectedFunctional)
-			return generateFunctional;
-		
-		return generateInterop;
+		}
 	}
 	
-	private Boolean setConfigurationAndContinue() {
+	private Boolean getConfigurationAndContinue() {
 
 		TestdataOptionsSelectionDialog dialog= new TestdataOptionsSelectionDialog(Display.getCurrent().getActiveShell());
 		int returnStatus =  dialog.open();
-		
+
 		configKeys = dialog.getConfigKeys();
-		
+
 		return returnStatus == 0;
 	}
+	
+	private String getGenerationType(){
 
+		Boolean selectedInterop = new Boolean(configKeys.get("selectedInterop"));
+		Boolean selectedFunctional = new Boolean(configKeys.get("selectedFunctional"));
+
+		if(selectedFunctional && selectedInterop)
+			return generateAll;
+
+		if ( selectedFunctional)
+			return generateFunctional;
+
+		return generateInterop;
+	}
+	
 }
+
