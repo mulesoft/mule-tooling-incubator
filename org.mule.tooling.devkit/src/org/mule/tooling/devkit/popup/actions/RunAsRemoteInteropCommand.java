6811e86cbd4e307b78a9c196aaaa099a3774f0e0
AleGMarra <alemarra@gmail.com>
2014-04-23 08:33:14 -0300

    Migrated http client to apache http 4.3

--- org.mule.tooling.devkit/src/org/mule/tooling/devkit/popup/actions/RunAsRemoteInteropCommand.java
+++ org.mule.tooling.devkit/src/org/mule/tooling/devkit/popup/actions/RunAsRemoteInteropCommand.java
@@ -1,25 +1,20 @@
 package org.mule.tooling.devkit.popup.actions;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.ConnectException;
 import java.util.Map;
 
-import javax.management.RuntimeErrorException;
-
-import org.apache.commons.httpclient.params.HttpParams;
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.mime.MultipartEntity;
-import org.apache.http.entity.mime.content.ContentBody;
-import org.apache.http.entity.mime.content.FileBody;
-import org.apache.http.entity.mime.content.StringBody;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -27,22 +22,21 @@ import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.mule.tooling.devkit.common.DevkitUtils;
 import org.mule.tooling.devkit.dialogs.InteropRunOptionsSelectionDialog;
-import org.mule.tooling.devkit.dialogs.TestdataOptionsSelectionDialog;
 
 
 public class RunAsRemoteInteropCommand extends AbstractHandler{
 
 	public enum ConfigKeys {destinationEmail, testDataPath, testDataOverridePath,
-		repository, serverURL, selectedDebug, selectedVerbose};
+		repository, serverURL, selectedDebug, selectedVerbose, selectedWindows, selectedLinux};
 
 		private Map<ConfigKeys, String> runnerConfig = null;
 
@@ -61,26 +55,35 @@ public class RunAsRemoteInteropCommand extends AbstractHandler{
 					if (selectedProject != null) {
 
 						setTestdataFilesInProjectAsDefault(selectedProject);
-
+						
 						if ( getConfigurationAndContinue() ){
 
 							try {
-								DefaultHttpClient httpclient = new DefaultHttpClient();
+								HttpClient client = HttpClientBuilder.create().build();
 
 								HttpPost httppost = createPostRequest();
 								
 								System.out.println("executing request " + httppost.getRequestLine());
 								
-								HttpResponse response = httpclient.execute(httppost);
+								HttpResponse response = client.execute(httppost);
+								showResponseInformationDialog(response);
 
 								System.out.println(response.getEntity().toString());
-
+								
+							} catch (ConnectException e){
+								e.printStackTrace();
+								MessageDialog.openError(Display.getCurrent().getActiveShell(), "Unexpected error occurred", e.getLocalizedMessage());
+								throw new RuntimeException(e.getMessage());
+								
 							} catch (ClientProtocolException e) {
 								e.printStackTrace();
-								throw new RuntimeException();
+								MessageDialog.openError(Display.getCurrent().getActiveShell(), "Unexpected error occurred", e.getLocalizedMessage());
+								throw new RuntimeException(e.getMessage());
+								
 							} catch (IOException e) {
 								e.printStackTrace();
-								throw new RuntimeException();
+								MessageDialog.openError(Display.getCurrent().getActiveShell(), "Unexpected error occurred", e.getLocalizedMessage());
+								throw new RuntimeException(e.getMessage());
 							}
 						}	
 
@@ -90,6 +93,31 @@ public class RunAsRemoteInteropCommand extends AbstractHandler{
 
 
 			return null;
+		}
+
+
+		private void showResponseInformationDialog(HttpResponse response)
+				throws IOException 
+		{
+			String message = EntityUtils.toString(response.getEntity());
+			
+			switch (response.getStatusLine().getStatusCode()) {
+			case HttpStatus.SC_OK:
+				MessageDialog.openInformation(Display.getCurrent().getActiveShell(), "Server response", message);
+				break;
+
+			case HttpStatus.SC_NOT_FOUND:
+				MessageDialog.openError(Display.getCurrent().getActiveShell(), "Server response", "An unexpected error occurred: " + response.getStatusLine());
+				break;
+
+			case HttpStatus.SC_REQUEST_TIMEOUT:
+				MessageDialog.openError(Display.getCurrent().getActiveShell(), "Server Timeout", "The connection timed out");
+				break;
+
+			default :
+				MessageDialog.openError(Display.getCurrent().getActiveShell(), "Unexpected error", "An unexpected error occurred: " + response.getStatusLine());
+				break;
+			}	
 		}
 
 
@@ -104,7 +132,6 @@ public class RunAsRemoteInteropCommand extends AbstractHandler{
 				
 				IContainer testResourcesFolder = selectedProject.getFolder(DevkitUtils.TEST_RESOURCES_FOLDER);
 				IContainer generatedResourcesFolder = testResourcesFolder.getFolder(new Path("generated"));
-				
 				
 				testDataPath = findResourceInFolder(generatedResourcesFolder, testDataNameFormat);
 				testDataOverridePath = findResourceInFolder(generatedResourcesFolder, testDataOverrideNameFormat);
@@ -128,32 +155,30 @@ public class RunAsRemoteInteropCommand extends AbstractHandler{
 
 
 		private HttpPost createPostRequest() throws UnsupportedEncodingException {
+			
 			HttpPost httppost = new HttpPost(runnerConfig.get(ConfigKeys.serverURL));
-
 			File testData = new File(runnerConfig.get(ConfigKeys.testDataPath));
 			File override = new File(runnerConfig.get(ConfigKeys.testDataOverridePath));
+			
+			MultipartEntityBuilder mpBuilder = MultipartEntityBuilder.create();        
 
-			MultipartEntity mpEntity = new MultipartEntity();
+			
+			mpBuilder.addTextBody("email", runnerConfig.get(ConfigKeys.destinationEmail));
 
-			mpEntity.addPart("email", 
-					new StringBody(runnerConfig.get(ConfigKeys.destinationEmail)));
+			mpBuilder.addTextBody("debug", runnerConfig.get(ConfigKeys.selectedDebug));
 
-			mpEntity.addPart("debug", 
-					new StringBody(runnerConfig.get(ConfigKeys.selectedDebug)));
+			mpBuilder.addTextBody("logLevel", getLogLevel());
 
-			mpEntity.addPart("logLevel", new StringBody(getLogLevel()));
+			mpBuilder.addTextBody("connectorRepository", runnerConfig.get(ConfigKeys.repository));
 
-			mpEntity.addPart("connectorRepository", 
-					new StringBody(runnerConfig.get(ConfigKeys.repository)));
-
-			mpEntity.addPart("testDataFile", new FileBody(testData, "Application/xml"));
-			mpEntity.addPart("testDataOverrideFile", new FileBody(override, "Application/xml"));
+			mpBuilder.addBinaryBody("testDataFile", testData, ContentType.APPLICATION_XML, testData.getName());
+			mpBuilder.addBinaryBody("testDataOverrideFile", override, ContentType.APPLICATION_XML, override.getName());
 
 
-			httppost.setEntity(mpEntity);
+			httppost.setEntity(mpBuilder.build());
+			
 			return httppost;
 		}
-
 
 		private String getLogLevel(){
 			return ( new Boolean(runnerConfig.get("selectedVerbose")) ? "verbose": "info" );
@@ -165,7 +190,7 @@ public class RunAsRemoteInteropCommand extends AbstractHandler{
 			int returnStatus =  dialog.open();
 
 			runnerConfig = dialog.getConfigKeys();
-
+			
 			return returnStatus == 0;
 		}
 		
