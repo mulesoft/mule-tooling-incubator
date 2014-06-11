e9ad31b00d9286b2df8eae18191e543bbeec27f9
pablocabrera85 <pablo.cabrera@mulesoft.com>
2014-06-11 12:35:27 -0300

    Refresh generated folder after generating sources

--- org.mule.tooling.devkit/src/org/mule/tooling/devkit/popup/actions/GenerateSourcesCommand.java
+++ org.mule.tooling.devkit/src/org/mule/tooling/devkit/popup/actions/GenerateSourcesCommand.java
@@ -19,86 +19,70 @@ import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.mule.tooling.devkit.common.DevkitUtils;
 import org.mule.tooling.devkit.maven.BaseDevkitGoalRunner;
 import org.mule.tooling.devkit.maven.MavenDevkitProjectDecorator;
 
 public class GenerateSourcesCommand extends AbstractHandler {
 
-	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		ISelection selection = HandlerUtil.getActiveWorkbenchWindow(event)
-				.getActivePage().getSelection();
-		if (selection != null & selection instanceof IStructuredSelection) {
-			Object selected = ((IStructuredSelection) selection)
-					.getFirstElement();
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        ISelection selection = HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().getSelection();
+        if (selection != null & selection instanceof IStructuredSelection) {
+            Object selected = ((IStructuredSelection) selection).getFirstElement();
 
-			if (selected instanceof IJavaElement) {
-				final IProject selectedProject = ((IJavaElement) selected)
-						.getJavaProject().getProject();
-				if (selectedProject != null) {
-					int errorCount = getErrorsCount(selectedProject);
+            if (selected instanceof IJavaElement) {
+                final IProject selectedProject = ((IJavaElement) selected).getJavaProject().getProject();
+                if (selectedProject != null) {
+                    int errorCount = getErrorsCount(selectedProject);
 
-					if (errorCount > 0) {
-						String errorText = "Your project has (" + errorCount + ") "
-								+ ((errorCount > 1) ? "errors" : "error" + ".");
-						boolean result = MessageDialog
-								.openConfirm(
-										null,
-										"Warning",
-										errorText
-												+ "\n\nDo you want to continue with this operation?.");
-						if (!result) {
-							return null;
-						}
-					}
+                    if (errorCount > 0) {
+                        String errorText = "Your project has (" + errorCount + ") " + ((errorCount > 1) ? "errors" : "error" + ".");
+                        boolean result = MessageDialog.openConfirm(null, "Warning", errorText + "\n\nDo you want to continue with this operation?.");
+                        if (!result) {
+                            return null;
+                        }
+                    }
 
-					final String convertingMsg = "Generating Sources...";
-					final WorkspaceJob changeClasspathJob = new WorkspaceJob(
-							convertingMsg) {
+                    final String convertingMsg = "Generating Sources...";
+                    final WorkspaceJob changeClasspathJob = new WorkspaceJob(convertingMsg) {
 
-						@Override
-						public IStatus runInWorkspace(final IProgressMonitor monitor)
-								throws CoreException {
-							monitor.beginTask(convertingMsg, 100);
-							IJavaProject javaProject= JavaCore.create(selectedProject);
-							MavenDevkitProjectDecorator mavenProject = MavenDevkitProjectDecorator
-									.decorate(javaProject);
-							new BaseDevkitGoalRunner(new String[] { "clean",
-									"package", "-DskipTests",
-									"-Ddevkit.studio.package.skip=true",
-									"-Ddevkit.javadoc.check.skip=true",
-									"-Dmaven.javadoc.skip=true" },javaProject).run(
-									mavenProject.getPomFile(), monitor);
-							monitor.done();
-							return Status.OK_STATUS;
-						}
-					};
-					changeClasspathJob.setUser(true);
-					changeClasspathJob.setPriority(Job.SHORT);
-					changeClasspathJob.schedule();
+                        @Override
+                        public IStatus runInWorkspace(final IProgressMonitor monitor) throws CoreException {
+                            monitor.beginTask(convertingMsg, 100);
+                            IJavaProject javaProject = JavaCore.create(selectedProject);
+                            MavenDevkitProjectDecorator mavenProject = MavenDevkitProjectDecorator.decorate(javaProject);
+                            new BaseDevkitGoalRunner(new String[] { "clean", "package", "-DskipTests", "-Ddevkit.studio.package.skip=true", "-Ddevkit.javadoc.check.skip=true",
+                                    "-Dmaven.javadoc.skip=true" }, javaProject).run(mavenProject.getPomFile(), monitor);
+                            DevkitUtils.refreshFolder(selectedProject.getFolder(DevkitUtils.GENERATED_SOURCES_FOLDER), monitor).execute();
+                            monitor.done();
+                            return Status.OK_STATUS;
+                        }
+                    };
+                    changeClasspathJob.setUser(true);
+                    changeClasspathJob.setPriority(Job.SHORT);
+                    changeClasspathJob.schedule();
 
-				}
-			}
-		}
-		return null;
-	}
+                }
+            }
+        }
+        return null;
+    }
 
-	private int getErrorsCount(final IProject selectedProject) {
-		int errorCount = 0;
-		IMarker[] errors;
-		try {
-			errors = selectedProject.findMarkers(null /* all markers */,
-					true, IResource.DEPTH_INFINITE);
-			for (IMarker error : errors) {
-				int severity = error.getAttribute(IMarker.SEVERITY,
-						Integer.MAX_VALUE);
-				if (severity == IMarker.SEVERITY_ERROR) {
-					errorCount++;
-				}
-			}
-		} catch (CoreException e1) {
-			e1.printStackTrace();
-		}
-		return errorCount;
-	}
+    private int getErrorsCount(final IProject selectedProject) {
+        int errorCount = 0;
+        IMarker[] errors;
+        try {
+            errors = selectedProject.findMarkers(null /* all markers */, true, IResource.DEPTH_INFINITE);
+            for (IMarker error : errors) {
+                int severity = error.getAttribute(IMarker.SEVERITY, Integer.MAX_VALUE);
+                if (severity == IMarker.SEVERITY_ERROR) {
+                    errorCount++;
+                }
+            }
+        } catch (CoreException e1) {
+            e1.printStackTrace();
+        }
+        return errorCount;
+    }
 }
