4cb5dc7f8391555237501d2999bff87017aeb6b0
pablocabrera85 <pablo.cabrera@mulesoft.com>
2014-07-16 15:16:53 -0300

    Fix DEVKIT-776. Programatically reset perspective at installation or update

--- org.mule.tooling.devkit/src/org/mule/tooling/devkit/DevkitUIPlugin.java
+++ org.mule.tooling.devkit/src/org/mule/tooling/devkit/DevkitUIPlugin.java
@@ -3,19 +3,38 @@ package org.mule.tooling.devkit;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.ui.progress.WorkbenchJob;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Version;
 
 public class DevkitUIPlugin extends AbstractUIPlugin {
 
     public static final String PLUGIN_ID = "org.mule.tooling.devkit";
+    private static final String PREFERENCES_CURRENT_UI_PLUGIN_VERSION = "devkit_ui_preference_current_ui_plugin_version";
     private static DevkitUIPlugin plugin;
 
     public void start(BundleContext context) throws Exception {
         super.start(context);
         plugin = this;
+        scheduleResetPerspectiveOnUpdateJob();
+    }
+
+    private void scheduleResetPerspectiveOnUpdateJob() {
+        IPreferenceStore store = getPreferenceStore();
+
+        String storedUiPluginVersion = store.getString(PREFERENCES_CURRENT_UI_PLUGIN_VERSION);
+        Version storedVersion = Version.parseVersion(storedUiPluginVersion);
+        Version currentVersion = this.getBundle().getVersion();
+
+        if (currentVersion.compareTo(storedVersion) != 0) {
+            WorkbenchJob resetPerspectiveJob = new ResetMulePerspectiveJob("Reset perspective on update");
+            resetPerspectiveJob.schedule();
+            store.setValue(PREFERENCES_CURRENT_UI_PLUGIN_VERSION, currentVersion.toString());
+        }
     }
 
     public void stop(BundleContext context) throws Exception {
