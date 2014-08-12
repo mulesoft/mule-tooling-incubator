25a179916ed2e168c0e6da19ddec00baf8068b8a
pablocabrera85 <pablo.cabrera@mulesoft.com>
2014-08-12 18:20:50 -0300

    Add java standard items for the popup menu

--- org.mule.tooling.devkit/src/org/mule/tooling/devkit/perspective/DevkitPerspectiveFactory.java
+++ org.mule.tooling.devkit/src/org/mule/tooling/devkit/perspective/DevkitPerspectiveFactory.java
@@ -1,8 +1,13 @@
 package org.mule.tooling.devkit.perspective;
 
+import org.eclipse.debug.ui.IDebugUIConstants;
+import org.eclipse.jdt.ui.JavaUI;
+import org.eclipse.search.ui.NewSearchUI;
 import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.console.IConsoleConstants;
+import org.eclipse.ui.progress.IProgressConstants;
 import org.mule.tooling.devkit.treeview.DevkitView;
 
 public class DevkitPerspectiveFactory implements IPerspectiveFactory {
@@ -15,11 +20,32 @@ public class DevkitPerspectiveFactory implements IPerspectiveFactory {
         // Top left: Resource Navigator view and Bookmarks view placeholder
         IFolderLayout topLeft = layout.createFolder("topLeft", IPageLayout.LEFT, 0.25f,
                 editorArea);
-        topLeft.addView("org.eclipse.jdt.ui.PackageExplorer");
+        topLeft.addView(JavaUI.ID_PACKAGES);
         topLeft.addPlaceholder(IPageLayout.ID_BOOKMARKS);
 
         // Bottom right: Task List view
         layout.addView(IPageLayout.ID_PROBLEM_VIEW, IPageLayout.BOTTOM, 0.66f, editorArea);
+        
+        layout.addActionSet(IDebugUIConstants.LAUNCH_ACTION_SET);
+        layout.addActionSet(JavaUI.ID_ACTION_SET);
+        layout.addActionSet(JavaUI.ID_ELEMENT_CREATION_ACTION_SET);
+        layout.addActionSet(IPageLayout.ID_NAVIGATE_ACTION_SET);
+
+        // views - java
+        layout.addShowViewShortcut(JavaUI.ID_PACKAGES);
+
+        // views - search
+        layout.addShowViewShortcut(NewSearchUI.SEARCH_VIEW_ID);
+
+        // views - debugging
+        layout.addShowViewShortcut(IConsoleConstants.ID_CONSOLE_VIEW);
+
+        // views - standard workbench
+        layout.addShowViewShortcut(IPageLayout.ID_OUTLINE);
+        layout.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);
+        layout.addShowViewShortcut(IPageLayout.ID_TASK_LIST);
+        layout.addShowViewShortcut(IProgressConstants.PROGRESS_VIEW_ID);
+        layout.addShowViewShortcut(IPageLayout.ID_PROJECT_EXPLORER);
         
         // RIGT left: Resource Navigator view and Bookmarks view placeholder
         IFolderLayout topRight = layout.createFolder("topRight", IPageLayout.RIGHT, 0.77f,
