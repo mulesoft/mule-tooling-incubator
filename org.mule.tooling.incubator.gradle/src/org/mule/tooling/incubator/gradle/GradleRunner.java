5853367aaec70bd446701740e189b649c3311a21
Juan Alberto López Cavallotti <juancavallotti@gmail.com>
2014-10-02 11:12:40 -0400

    Improved feedback when running a build.

--- org.mule.tooling.incubator.gradle/src/org/mule/tooling/incubator/gradle/GradleRunner.java
+++ org.mule.tooling.incubator.gradle/src/org/mule/tooling/incubator/gradle/GradleRunner.java
@@ -5,11 +5,13 @@ import org.eclipse.ui.console.ConsolePlugin;
 import org.eclipse.ui.console.IOConsoleOutputStream;
 import org.eclipse.ui.console.MessageConsole;
 import org.gradle.tooling.BuildLauncher;
+import org.gradle.tooling.ProgressEvent;
+import org.gradle.tooling.ProgressListener;
 import org.mule.tooling.ui.utils.UiUtils;
 
 public class GradleRunner {
 
-    public static void run(final BuildLauncher build, IProgressMonitor monitor, String... runArgs) {
+    public static void run(final BuildLauncher build, final IProgressMonitor monitor, String... runArgs) {
         monitor.beginTask("Running gradle build", IProgressMonitor.UNKNOWN);
         MessageConsole messageConsole = UiUtils.getMessageConsole("Gradle run");
         final IOConsoleOutputStream consoleStream = messageConsole.newOutputStream();
@@ -19,6 +21,15 @@ public class GradleRunner {
         UiUtils.showConsoleView();
         // STUDIO-2676 - bring new console to front
         ConsolePlugin.getDefault().getConsoleManager().showConsoleView(messageConsole);
+        
+        build.addProgressListener(new ProgressListener() {
+            
+            @Override
+            public void statusChanged(ProgressEvent progressEvent) {
+                monitor.beginTask(progressEvent.getDescription(), IProgressMonitor.UNKNOWN);
+            }
+        });
+        
         build.run();
         monitor.done();
     }
