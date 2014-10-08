2bd9415dc7b8dad17a7abe3160c5f46ef43365ea
Juan Alberto López Cavallotti <juancavallotti@gmail.com>
2014-10-08 19:01:10 -0300

    added AST parsing to content descriptor to improve mule-enabled gradle build scripts

--- org.mule.tooling.incubator.gradle/src/org/mule/tooling/incubator/gradle/content/StudioGradleEnabledContentDescriber.java
+++ org.mule.tooling.incubator.gradle/src/org/mule/tooling/incubator/gradle/content/StudioGradleEnabledContentDescriber.java
@@ -1,33 +1,140 @@
 package org.mule.tooling.incubator.gradle.content;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.Properties;
 
+import org.codehaus.groovy.ast.CodeVisitorSupport;
+import org.codehaus.groovy.ast.expr.MapEntryExpression;
+import org.codehaus.groovy.ast.expr.MapExpression;
+import org.codehaus.groovy.ast.expr.MethodCallExpression;
+import org.codehaus.groovy.ast.expr.PropertyExpression;
+import org.codehaus.groovy.control.MultipleCompilationErrorsException;
 import org.eclipse.core.runtime.QualifiedName;
 import org.eclipse.core.runtime.content.IContentDescriber;
 import org.eclipse.core.runtime.content.IContentDescription;
 import org.eclipse.core.runtime.content.ITextContentDescriber;
+import org.mule.tooling.incubator.gradle.parser.ast.GradleScriptASTParser;
 
 
 public class StudioGradleEnabledContentDescriber implements ITextContentDescriber {
+    
+    private static class ContentDescriberVisitor extends CodeVisitorSupport {
+        
+        private static final String PLUGIN_NAME = "mulestudio";
+        
+        private static final String APPLY_METHOD_NAME = "apply";
+        
+        private static final String PROJECT_CONTEXT_NAME = "project";
+        
+        private static final String PLUGIN_KEY_NAME = "plugin";
+        
+        private static String IMPLEMENTATION_CLASS = "";
+        
+        static {
+            
+            try {
+                Properties props = new Properties();
+                props.load(StudioGradleEnabledContentDescriber.class.getResourceAsStream("/META-INF/gradle-plugins/"+PLUGIN_NAME+".properties"));
+                IMPLEMENTATION_CLASS = props.getProperty("implementation-class");
+            } catch (IOException ex) {
+                IMPLEMENTATION_CLASS = "";
+            }
+        }
+        
+        
+        private String lastPropertyAccess;
+        private boolean isApplyContext;
+        
+        private boolean foundplugin;
+        
+        @Override
+        public void visitMethodCallExpression(MethodCallExpression call) {
+            
+            if (!APPLY_METHOD_NAME.equals(call.getMethodAsString())) {
+                return;
+            }
+            
+            //we might be in some myprop.apply situation which we don't want
+            //so we try yo ensure that at least the last property access was called
+            //project, this is also not very reliable but is better than nothing.
+            
+            if (lastPropertyAccess != null && !PROJECT_CONTEXT_NAME.equals(lastPropertyAccess)) {
+                return;
+            }
+            
+            
+            isApplyContext = true;
+            
+            super.visitMethodCallExpression(call);
+            
+            //finished apply context
+            isApplyContext = false;
+        }
+        
+        @Override
+        public void visitMapExpression(MapExpression expression) {
+            
+            if (!isApplyContext) {
+                return;
+            }
+            
+            super.visitMapExpression(expression);
+        }
 
+        @Override
+        public void visitMapEntryExpression(MapEntryExpression expression) {
+            //if we are inside apply context
+            if (PLUGIN_KEY_NAME.equals(expression.getKeyExpression().getText())) {
+                String value = expression.getValueExpression().getText();
+                
+                foundplugin = PLUGIN_NAME.equals(value) || IMPLEMENTATION_CLASS.equals(value);                
+            }
+        }
+        
+        @Override
+        public void visitPropertyExpression(PropertyExpression expression) {
+            
+            if (!PROJECT_CONTEXT_NAME.equals(expression.getPropertyAsString())) {
+                //nothing interesting.
+                return;
+            }
+            
+            lastPropertyAccess = PROJECT_CONTEXT_NAME;
+            super.visitPropertyExpression(expression);
+            lastPropertyAccess = null;
+        }
+
+        
+        /**
+         * Return the truth about wether we found the plugin or not. Even though
+         * this might not be the ultimate source of truth given we're not applying 
+         * this script to a real gradle project.
+         * @return
+         */
+        public boolean isFoundplugin() {
+            return foundplugin;
+        }
+        
+    }
+    
+    
     @Override
     public int describe(InputStream contents, IContentDescription description) throws IOException {
         
-        BufferedReader rdr = new BufferedReader(new InputStreamReader(contents));
-        
-        String line = rdr.readLine();
-        
-        while (line != null) {
-            if (line.contains("apply") && line.contains("plugin") && line.contains("mulestudio")) {
+        try {
+            GradleScriptASTParser parser = new GradleScriptASTParser(contents);
+            ContentDescriberVisitor visitor = new ContentDescriberVisitor();
+            parser.walkScript(visitor);
+            
+            if (visitor.isFoundplugin()) {
                 return IContentDescriber.VALID;
             }
-            line = rdr.readLine();
+            
+        } catch (MultipleCompilationErrorsException ex) {
+            return IContentDescriber.INDETERMINATE;
         }
-        
         return IContentDescriber.INVALID;
     }
 
