978cf173dec5da72cddba94ff2f9eb6d1a9f4091
Mariano Achaval <mariano.achaval@mulesoft.com>
2014-10-23 10:50:23 -0300

    Removing installer

--- org.mule.tooling.incubator.installer/src/org/mule/tooling/incubator/installer/views/InstallerEventTypes.java
+++
@@ -1,8 +0,0 @@
-package org.mule.tooling.incubator.installer.views;
-
-public class InstallerEventTypes {
-
-    public static final String COMPLETE_EVENT_TYPE = "complete";
-    public static final String PROGRESS_EVENT_TYPE = "progress";
-    public static final String CANCELED_EVENT_TYPE = "canceled";
-}
