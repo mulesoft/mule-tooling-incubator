b9fc14f6a33c2d50aef627ae3e82058fa48f0384
Damian Sima <damiansima@gmail.com>
2014-12-18 18:01:08 -0300

    fix bug of throw-an for drawing

--- org.mule.tooling.incubator.munit.ui.contribution/src/org/mule/tooling/ui/contribution/munit/editors/custom/MockThrowCustomEditor.java
+++ org.mule.tooling.incubator.munit.ui.contribution/src/org/mule/tooling/ui/contribution/munit/editors/custom/MockThrowCustomEditor.java
@@ -3,13 +3,18 @@ package org.mule.tooling.ui.contribution.munit.editors.custom;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.ToolBarManager;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.ToolBar;
 import org.mule.tooling.messageflow.editor.MessageFlowEditor;
 import org.mule.tooling.model.messageflow.MessageFlowNode;
 import org.mule.tooling.model.messageflow.decorator.PropertyCollectionMap;
+import org.mule.tooling.ui.contribution.munit.MunitPlugin;
 import org.mule.tooling.ui.contribution.munit.editors.custom.mappers.MessageProcessorAttributeCollectionMapper;
 import org.mule.tooling.ui.contribution.munit.editors.custom.mappers.MunitMockModuleMapper;
 import org.mule.tooling.ui.modules.core.widgets.AttributesPropertyPage;
@@ -54,6 +59,46 @@ public class MockThrowCustomEditor extends MunitMockModuleCustomEditor {
 
 		GridLayoutFactory.fillDefaults().numColumns(4).equalWidth(false).applyTo(returnComposite);
 		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.FILL).span(ATTRIBUTE_VIEWER_COLUMNS, 1).grab(true, true).applyTo(returnComposite);
+		
+		
+		
+		ToolBar fieldToolBar = new ToolBar(returnComposite, SWT.NONE);
+        ToolBarManager fieldTBManager = new ToolBarManager(fieldToolBar);
+
+        final Composite returnPropertiesForm = new Composite(returnComposite, SWT.NONE);
+        GridDataFactory.fillDefaults().align(SWT.FILL, SWT.FILL).span(4, 1).grab(true, true).applyTo(returnPropertiesForm);
+        GridLayoutFactory.fillDefaults().numColumns(1).equalWidth(false).applyTo(returnPropertiesForm);
+
+//        propertiesTableViewer = new MockPropertiesTable(returnPropertiesForm, SWT.NULL, "Name", "Value", "Type");
+//        GridDataFactory.fillDefaults().align(SWT.FILL, SWT.FILL).span(3, 1).grab(true, true).applyTo(propertiesTableViewer);
+//        returnPropertiesForm.setVisible(false);
+        ((GridData) returnPropertiesForm.getLayoutData()).exclude = true;
+
+//        showReturnProperties = new Action("Add return message properties", Action.AS_CHECK_BOX) {
+//
+//            @Override
+//            public void run() {
+//                if (showReturnProperties.isChecked()) {
+//                    ((GridData) returnPropertiesForm.getLayoutData()).exclude = false;
+//                    returnPropertiesForm.setVisible(true);
+//
+//                    returnPropertiesForm.getParent().layout();
+//                    returnComposite.getParent().layout();
+//                } else {
+//                    ((GridData) returnPropertiesForm.getLayoutData()).exclude = true;
+//                    returnPropertiesForm.setVisible(false);
+//
+//                    returnPropertiesForm.getParent().layout();
+//                    returnComposite.getParent().layout();
+//
+//                }
+//            }
+//        };
+//
+//        showReturnProperties.setImageDescriptor(MunitPlugin.ZOOM_ICON_DESCRIPTOR);
+
+//        fieldTBManager.add(showReturnProperties);
+        fieldTBManager.update(true);
 	}
 
 	@Override
