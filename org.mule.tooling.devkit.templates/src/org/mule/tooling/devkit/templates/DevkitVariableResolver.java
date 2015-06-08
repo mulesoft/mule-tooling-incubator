4f1279088ddf4df9597324b88b0def925eaa8f72
pablocabrera85 <pablo.cabrera@mulesoft.com>
2015-06-08 17:59:04 -0300

    Use JDT Shared api to get compilation unit

--- org.mule.tooling.devkit.templates/src/org/mule/tooling/devkit/templates/DevkitVariableResolver.java
+++ org.mule.tooling.devkit.templates/src/org/mule/tooling/devkit/templates/DevkitVariableResolver.java
@@ -1,8 +1,6 @@
 package org.mule.tooling.devkit.templates;
 
 import org.eclipse.jdt.core.ICompilationUnit;
-import org.eclipse.jdt.core.dom.AST;
-import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.internal.corext.template.java.JavaContext;
 import org.eclipse.jdt.internal.corext.template.java.JavaContextType;
@@ -14,19 +12,21 @@ public class DevkitVariableResolver extends TemplateVariableResolver {
 
     protected String resolve(TemplateContext context) {
         if (context.getContextType().getId().equals(JavaContextType.ID_MEMBERS)) {
-            JavaContext javaContext = (JavaContext) context;
-            ICompilationUnit compilationUnit = javaContext.getCompilationUnit();
-            ASTParser parser = ASTParser.newParser(AST.JLS3);
-            parser.setKind(ASTParser.K_COMPILATION_UNIT);
-            parser.setSource(compilationUnit);
-            parser.setResolveBindings(true);
-            CompilationUnit cu = (CompilationUnit) parser.createAST(null);
-            LocateModuleNameVisitor visitor = new LocateModuleNameVisitor();
-            cu.accept(visitor);
-            if (!visitor.getValue().isEmpty()) {
-                return visitor.getValue();
+            if (context instanceof JavaContext) {
+                JavaContext javaContext = (JavaContext) context;
+                ICompilationUnit compilationUnit = javaContext.getCompilationUnit();
+                CompilationUnit cu = parse(compilationUnit);
+                LocateModuleNameVisitor visitor = new LocateModuleNameVisitor();
+                cu.accept(visitor);
+                if (!visitor.getValue().isEmpty()) {
+                    return visitor.getValue();
+                }
             }
         }
         return super.resolve(context);
+    }
+
+    private CompilationUnit parse(ICompilationUnit unit) {
+        return org.eclipse.jdt.ui.SharedASTProvider.getAST(unit, org.eclipse.jdt.ui.SharedASTProvider.WAIT_YES, null);
     }
 }
