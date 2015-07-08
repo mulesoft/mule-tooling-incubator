44fe834a4dcbc23c251ff25968b2c0482d2fcf6c
pablocabrera85 <pablo.cabrera@mulesoft.com>
2015-07-08 13:41:34 -0300

    Move to the next version

--- org.mule.tooling.devkit.apt/src/org/mule/tooling/devkit/apt/StudioAnnotationProcessor.java
+++ org.mule.tooling.devkit.apt/src/org/mule/tooling/devkit/apt/StudioAnnotationProcessor.java
@@ -38,7 +38,7 @@ import org.mule.tooling.devkit.apt.plugin.StudioPluginScanner;
 public class StudioAnnotationProcessor extends AnnotationProcessor {
 
     public static final String ENABLED_DEVKIT_CHECK_VERSION = "enabledDevKitCheckVersion";
-    private static final String SUPPORTED_VERSION = "3.7.0-SNAPSHOT";
+    private static final String SUPPORTED_VERSION = "3.7.1-SNAPSHOT";
 
     @Override
     public void init(ProcessingEnvironment env) {
