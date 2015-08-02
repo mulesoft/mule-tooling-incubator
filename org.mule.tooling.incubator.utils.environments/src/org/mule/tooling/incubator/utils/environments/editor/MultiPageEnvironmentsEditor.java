6b239d054df9948a261f722893c0b88d9524b980
Juan Alberto López Cavallotti <juancavallotti@gmail.com>
2015-08-02 12:09:46 -0300

    removed unused import

--- org.mule.tooling.incubator.utils.environments/src/org/mule/tooling/incubator/utils/environments/editor/MultiPageEnvironmentsEditor.java
+++ org.mule.tooling.incubator.utils.environments/src/org/mule/tooling/incubator/utils/environments/editor/MultiPageEnvironmentsEditor.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Collections;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Properties;
