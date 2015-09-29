f0137fa54c476f7fa1aa58882e2a1291696dcdea
Mariano Capurro <mcapurro@gmail.com>
2015-09-29 06:57:42 -0700

    Added latest changes mainly PREFETCH support

--- org.mule.tooling.incubator.messaging.contribution/src/org/mule/tooling/messaging/editor/global/booleanloaders/PrefetchLoadedValueModifier.java
+++ org.mule.tooling.incubator.messaging.contribution/src/org/mule/tooling/messaging/editor/global/booleanloaders/PrefetchLoadedValueModifier.java
@@ -6,18 +6,18 @@ import java.util.Set;
 
 import org.apache.commons.lang.StringUtils;
 
-public class AccessLoadedValueModifier extends Abstract3WayLoadedValueModifierTemplate {
-    // Access
-    public static final String ACCESS_NAMESPACE = "http://www.mulesoft.org/schema/mule/mq/access";
-    public static final String ACCESS_STORE_ID_PREFIX = "@" + ACCESS_NAMESPACE + ";";
-    public static final String ACCESS_REFERENCE_PROPERTY_ID = "access-ref";
-    public static final String ACCESS_REFERENCE_BE_ID = "reference";
-    public static final String ACCESS_NESTED_BE_ID = "nested";
+public class PrefetchLoadedValueModifier extends Abstract3WayLoadedValueModifierTemplate {
+    // Prefetched
+    public static final String PREFETCH_NAMESPACE = "http://www.mulesoft.org/schema/mule/mq/prefetched";
+    public static final String PREFETCH_STORE_ID_PREFIX = "@" + PREFETCH_NAMESPACE + ";";
+    public static final String PREFETCH_REFERENCE_PROPERTY_ID = "prefetch-ref";
+    public static final String PREFETCH_REFERENCE_BE_ID = "reference";
+    public static final String PREFETCH_NESTED_BE_ID = "nested";
     
     protected Boolean haveNestedConfiguration(PropertyCollectionMap props) {
         Set<String> keySet = props.getPropertyCollections().keySet();
         for (String propertyCollectionName : keySet) {
-            if (propertyCollectionName.startsWith(ACCESS_STORE_ID_PREFIX)) {
+            if (propertyCollectionName.startsWith(PREFETCH_STORE_ID_PREFIX)) {
                 return true;
             }
         }
@@ -25,23 +25,22 @@ public class AccessLoadedValueModifier extends Abstract3WayLoadedValueModifierTe
     }
 
     protected Boolean haveGlobalRefConfiguration(PropertyCollectionMap props) {
-        String tlsContextRef = props.getProperty(ACCESS_REFERENCE_PROPERTY_ID, "");
+        String tlsContextRef = props.getProperty(PREFETCH_REFERENCE_PROPERTY_ID, "");
         return StringUtils.isNotBlank(tlsContextRef);
     }
 
     @Override
     protected String getNestedRadioBooleanId() {
-        return ACCESS_NESTED_BE_ID;
+        return PREFETCH_NESTED_BE_ID;
     }
 
     @Override
     protected String getGlobalRefRadioBooleanId() {
-        return ACCESS_REFERENCE_BE_ID;
+        return PREFETCH_REFERENCE_BE_ID;
     }
 
     @Override
-    protected String getNoneConfigurationRadioBooleanId()
-    {
+    protected String getNoneConfigurationRadioBooleanId() {
         return null;
     }
 
